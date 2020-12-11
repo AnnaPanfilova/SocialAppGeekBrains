65f2b88f9cab97e03af9710a4a4d5f5ef5632e23
Anna <anna.panfilova2107@gmail.com>
2020-12-11 18:17:04 +0300

    Домашнее задание 3 #2

---
+++ SocialApp/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  SocialApp
+//
+//  Created by Anna on 10.12.2020.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+}
